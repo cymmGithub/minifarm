488dbe6fd02d567fb14ea9a53a3d99e1d1a2b0d4
Przemek Swiercz <przemek.swiercz@mindyra.com>
2026-05-13 08:55:49 +0200

    Add genericized AboutSection with public header bar

--- about/src/main.tsx
+++ about/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { AboutSection } from './AboutSection';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="font-mono text-amber-500/80 p-8">
-      hello minifarm about
-    </div>
+    <AboutSection />
   </StrictMode>
 );
